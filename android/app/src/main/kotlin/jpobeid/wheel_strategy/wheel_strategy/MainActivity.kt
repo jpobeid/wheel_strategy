dcef1390929223e4e97a900a60bb742283283ef7
JP Obeid <jpobeid91@gmail.com>
2024-01-23 14:26:30 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/jpobeid/wheel_strategy/wheel_strategy/MainActivity.kt
@@ -0,0 +1,6 @@
+package jpobeid.wheel_strategy.wheel_strategy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
